b9b2bae94d75c7c3ebfc6939511a19071bb86a12
mulonemartin <mulone.martin@gmail.com>
2022-04-04 11:16:02 -0300

    Bump version 3.0.2

--- indexer/tasks.py
+++ indexer/tasks.py
@@ -22,7 +22,7 @@ from .scan_moc_user import ScanUser
 from .scan_utils import BlockchainUtils
 
 
-__VERSION__ = '3.0.1'
+__VERSION__ = '3.0.2'
 
 log.info("Starting MoC Indexer version {0}".format(__VERSION__))
 
