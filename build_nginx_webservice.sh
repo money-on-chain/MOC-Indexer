1e8f6671da826af75947b3d67ac1728531dcadb1
mulonemartin <mulone.martin@gmail.com>
2022-07-19 14:45:14 -0300

    Remove unused

--- build_nginx_webservice.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# exit as soon as an error happen
-set -e
-
-# Create image that will compile the app into a bundle
-docker build -t nginx_api_ipfs -f webservice/Docker/Dockerfile.nginx .
